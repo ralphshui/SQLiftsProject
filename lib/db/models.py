67990295cb3d4ccf00c67d65f5ab1c2d93963ac6
Ralph Shui <rskc.2112@gmail.com>
2023-08-09 17:12:30 -0400

    set up models

---
+++ lib/db/models.py
@@ -0,0 +1,47 @@
+from sqlalchemy import Column, Integer, PrimaryKeyConstraint, String
+from sqlalchemy.orm import declarative_base
+
+import click
+
+Base = declarative_base()
+
+class Name(Base):
+    __tablename__= 'names'
+    __table_args__= (PrimaryKeyConstraint("id"),)
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+
+    def __repr__(self):
+        return f"Id: {self.id}, "\
+        + f"{click.style('Name:', fg='green', bold=True)} {self.name}"
+
+class Exercise(Base):
+    __tablename__= 'Exercises'
+    __table_args__= (PrimaryKeyConstraint("id"),)
+
+    id = Column(Integer(), primary_key=True)
+    exercise = Column(String())
+
+    def __repr__(self):
+        return f"Id: {self.id}, "\
+        + f"{click.style('Exercise:', fg='green', bold=True)} {self.exercise}"
+
+class Preference(Base):
+    __tablename__= 'preferences'
+    __table_args__= (PrimaryKeyConstraint("id"),)
+
+    id = Column(Integer(), primary_key=True)
+    workout_day = Column(String())
+    exercise = Column(String())
+    reps = Column(Integer())
+    sets = Column(Integer())
+    weight = Column(Integer())
+ 
+    def __repr__(self):
+        return f"Id: {self.id}, "\
+        + f"{click.style('Day:', fg='green', bold=True)} {self.workout_day}, "\
+        + f"{click.style('Name:', fg='green', bold=True)} {self.exercise}, "\
+        + f"{click.style('Reps:', fg='green', bold=True)} {self.reps}, "\
+        + f"{click.style('Sets:', fg='green', bold=True)} {self.sets}, "\
+        + f"{click.style('Weight:', fg='green', bold=True)} {self.weight}"
--- lib/workout.py
+++ lib/workout.py
@@ -5,21 +5,46 @@ import click
 
 Base = declarative_base()
 
-class Workout(Base):
-    __tablename__= 'workouts'
+class Name(Base):
+    __tablename__= 'names'
     __table_args__= (PrimaryKeyConstraint("id"),)
 
-    id = Column(Integer())
-    workout_day = Column(String())
-    workout_name = Column(String())
-    reps = Column(Integer())
-    sets = Column(Integer())
-    weight = Column(Integer())
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
 
     def __repr__(self):
         return f"Id: {self.id}, "\
+        + f"{click.style('Name:', fg='green', bold=True)} {self.name}"
+
+class Exercise(Base):
+    __tablename__= 'Exercises'
+    __table_args__= (PrimaryKeyConstraint("id"),)
+
+    id = Column(Integer(), primary_key=True)
+    exercise = Column(String())
+
+    def __repr__(self):
+        return f"Id: {self.id}, "\
+        + f"{click.style('Exercise:', fg='green', bold=True)} {self.exercise}"
+
+class Preference(Base):
+    __tablename__= 'preferences'
+    __table_args__= (PrimaryKeyConstraint("id"),)
+
+    id = Column(Integer(), primary_key=True)
+    workout_day = Column(String())
+    exercise = Column(String())
+    reps = Column(Integer())
+    sets = Column(Integer())
+    weight = Column(Integer())
+ 
+    def __repr__(self):
+        return f"Id: {self.id}, "\
         + f"{click.style('Day:', fg='green', bold=True)} {self.workout_day}, "\
-        + f"{click.style('Name:', fg='green', bold=True)} {self.workout_name}, "\
+        + f"{click.style('Name:', fg='green', bold=True)} {self.exercise}, "\
         + f"{click.style('Reps:', fg='green', bold=True)} {self.reps}, "\
         + f"{click.style('Sets:', fg='green', bold=True)} {self.sets}, "\
         + f"{click.style('Weight:', fg='green', bold=True)} {self.weight}"
+
+
+
